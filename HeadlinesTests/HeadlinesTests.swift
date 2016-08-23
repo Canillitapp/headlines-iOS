9def33e474f032a3b49addb7c748ccd00a8401ad
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2016-08-23 00:13:46 -0300

    Initial Commit

---
+++ HeadlinesTests/HeadlinesTests.swift
@@ -0,0 +1,36 @@
+//
+//  HeadlinesTests.swift
+//  HeadlinesTests
+//
+//  Created by Ezequiel Becerra on 8/23/16.
+//  Copyright © 2016 Ezequiel Becerra. All rights reserved.
+//
+
+import XCTest
+@testable import Headlines
+
+class HeadlinesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
