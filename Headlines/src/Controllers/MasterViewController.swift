cd1517ab8f1c7f2cf9b7dc25c9d3cc27d049409a
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2016-08-23 02:09:03 -0300

    Getting data from server

--- Headlines/src/Controllers/MasterViewController.swift
+++ Headlines/src/Controllers/MasterViewController.swift
@@ -10,51 +10,24 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var detailViewController: DetailViewController? = nil
     var objects = [AnyObject]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(insertNewObject(_:)))
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        let newsService = NewsService()
+        newsService.trendingNews { (result) in
+            guard let obj = result!["keywords"] as? [String] else {
+                return
+            }
+            self.objects = obj
+            self.tableView.reloadData()
         }
     }
 
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
-
-    // MARK: - Segues
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
     }
 
     // MARK: - Table View
@@ -70,8 +43,7 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        cell.textLabel!.text = objects[indexPath.row] as? String
         return cell
     }
 
--- Headlines/src/Services/NewsService.swift
+++ Headlines/src/Services/NewsService.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+class NewsService {
+    func trendingNews (completion: (result: AnyObject?) -> Void) {
+        Alamofire.request(.GET, "http://10.0.1.28:4567/trending/2016-08-16")
+            .responseJSON { response in
+
+                guard response.result.isSuccess else {
+                    completion(result: nil)
+                    return
+                }
+
+                completion(result: response.result.value)
+        }
+    }
+}
