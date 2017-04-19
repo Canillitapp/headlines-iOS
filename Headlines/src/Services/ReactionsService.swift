34a81766a2629b5ff08cc601ff2fd6db22568e5d
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2017-04-19 01:33:14 -0300

    Fetching user_id from CloudKit and sending it to backend

--- Headlines/src/Services/ReactionsService.swift
+++ Headlines/src/Services/ReactionsService.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CloudKit
 
 class ReactionsService: NSObject {
 
@@ -19,27 +20,44 @@ class ReactionsService: NSObject {
             return
         }
         
-        let url = URL(string: "http://45.55.247.52:4567/reactions/\(newsId)")
-        var request = URLRequest(url: url!)
-        request.httpMethod = "POST"
-        
-        let postString = "reaction=\(reaction)"
-        request.httpBody = postString.data(using: .utf8)
-        
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        
-        let task = session.dataTask(with: request, completionHandler: {(data, response, error) in
-            if let e = error {
-                fail?(e as NSError)
+        let container = CKContainer.default()
+        container.fetchUserRecordID { (recordId, error) in
+            if let err = error {
+                fail?(err)
                 return
             }
             
-            DispatchQueue.main.async(execute: {
-                success?(response)
+            guard let userId = recordId else {
+                let errUserInfo = [NSLocalizedDescriptionKey : "No user record id"]
+                let err = NSError(domain: NSStringFromClass(self.classForCoder),
+                                  code: 1,
+                                  userInfo: errUserInfo)
+                fail?(err)
+                return
+            }
+            
+            let url = URL(string: "http://127.0.0.1:4567/reactions/\(newsId)")
+            var request = URLRequest(url: url!)
+            request.httpMethod = "POST"
+            
+            let postString = "reaction=\(reaction)&source=iOS&user_id=\(userId.recordName)"
+            request.httpBody = postString.data(using: .utf8)
+            
+            let config = URLSessionConfiguration.default
+            let session = URLSession(configuration: config)
+            
+            let task = session.dataTask(with: request, completionHandler: {(data, response, error) in
+                if let e = error {
+                    fail?(e as NSError)
+                    return
+                }
+                
+                DispatchQueue.main.async(execute: {
+                    success?(response)
+                })
             })
-        })
-        
-        task.resume()
+            
+            task.resume()
+        }
     }
 }
