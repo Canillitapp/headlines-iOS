bb8e1595a197f2d7004e46d107264b7ebfcf6a71
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2017-04-27 01:03:36 -0300

    Adding touch event when tapping a reaction

--- Headlines/src/Controllers/MyReactionsViewController.swift
+++ Headlines/src/Controllers/MyReactionsViewController.swift
@@ -9,25 +9,49 @@
 import Foundation
 import UIKit
 import SDWebImage
+import SafariServices
 
 class MyReactionsViewController: UITableViewController {
     let reactionsService = ReactionsService()
     var reactions = [Reaction]()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    //  MARK: Private
+    func fetchMyReactions() {
+        refreshControl?.beginRefreshing()
         
         let success: (URLResponse?, [Reaction]) -> () = { response, reactions in
+            self.refreshControl?.endRefreshing()
+            
             self.reactions.removeAll()
             self.reactions.append(contentsOf: reactions)
             self.tableView.reloadData()
         }
         
         let fail: (Error) -> () = { error in
+            self.refreshControl?.endRefreshing()
+            
             print(error.localizedDescription)
         }
         
         reactionsService.getReactions(success: success, fail: fail)
+    }
+    
+    //  MARK: Public
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //  Setup refresh control
+        let refreshCtrl = UIRefreshControl()
+        tableView.refreshControl = refreshCtrl
+        
+        refreshCtrl.tintColor = UIColor(red:0.99, green:0.29, blue:0.39, alpha:1.00)
+        refreshCtrl.addTarget(self, action: #selector(fetchMyReactions), for: .valueChanged)
+        
+        //  Had to set content offset because of UIRefreshControl bug
+        //  http://stackoverflow.com/a/31224299/994129
+        tableView.contentOffset = CGPoint(x:0, y:-refreshCtrl.frame.size.height)
+        
+        fetchMyReactions()
     }
     
     //  MARK: UITableViewDataSource
@@ -41,7 +65,7 @@ class MyReactionsViewController: UITableViewController {
                 return UITableViewCell()
         }
         
-        let r = self.reactions[indexPath.row]
+        let r = reactions[indexPath.row]
         guard let n = r.news else {
             return cell
         }
@@ -51,5 +75,19 @@ class MyReactionsViewController: UITableViewController {
         cell.reactionImageView.sd_setImage(with: n.imageUrl)
         
         return cell
+    }
+    
+    //  MARK: UITableViewDelegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let r = reactions[indexPath.row]
+        
+        guard let n = r.news else {
+            return
+        }
+        
+        if let url = n.url {
+            let vc = SFSafariViewController(url: url, entersReaderIfAvailable: true)
+            present(vc, animated: true, completion: nil)
+        }
     }
 }
