508cee50b2f020d9a8b355b7d65f458178742876
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2017-06-05 03:06:57 -0300

    fix: tests

--- HeadlinesUITests/HeadlinesUIMockedTests.swift
+++ HeadlinesUITests/HeadlinesUIMockedTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class HeadlinesUITests: XCTestCase {
+class HeadlinesUIMockedTests: XCTestCase {
 
     let defaultWaitThreshold = 20.0
     
@@ -21,6 +21,7 @@ class HeadlinesUITests: XCTestCase {
         continueAfterFailure = false
 
         let app = XCUIApplication()
+        app.launchArguments.append("mockRequests")
         app.launch()
     }
 
@@ -29,36 +30,16 @@ class HeadlinesUITests: XCTestCase {
         super.tearDown()
     }
 
-    func testTrendingCards() {
-        let app = XCUIApplication()
-        let cell = app.collectionViews.cells.element(boundBy: 0)
-        
-        let exists = NSPredicate(format: "exists == 1")
-        let cellExistsExpectation = expectation(for: exists, evaluatedWith: cell, handler: nil)
-        wait(for: [cellExistsExpectation], timeout: defaultWaitThreshold)
-    }
-    
-    func testPopularNews() {
+    func testMockedReactions() {
         let app = XCUIApplication()
         
-        //  Go to Popular tab
-        app.tabBars.buttons["Popular"].tap()
+        //  Go to Reacciones tab
+        app.tabBars.buttons["Reacciones"].tap()
         
         let cell = app.tables.cells.element(boundBy: 0)
         let exists = NSPredicate(format: "exists == 1")
         let cellExistsExpectation = expectation(for: exists, evaluatedWith: cell, handler: nil)
         wait(for: [cellExistsExpectation], timeout: defaultWaitThreshold)
     }
-    
-    func testRecentNews() {
-        let app = XCUIApplication()
-        
-        //  Go to Reciente tab
-        app.tabBars.buttons["Reciente"].tap()
-        
-        let cell = app.tables.cells.element(boundBy: 0)
-        let exists = NSPredicate(format: "exists == 1")
-        let cellExistsExpectation = expectation(for: exists, evaluatedWith: cell, handler: nil)
-        wait(for: [cellExistsExpectation], timeout: defaultWaitThreshold)
-    }
+
 }
