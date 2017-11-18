a210b62cc12cb8b5eafee3346ee90b23dd56fa52
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2017-11-18 18:55:15 -0300

    subtle animation at loading screen

--- Headlines/src/Controllers/LoadingViewController.swift
+++ Headlines/src/Controllers/LoadingViewController.swift
@@ -8,31 +8,48 @@
 
 import UIKit
 
-class LoadingViewController: UIViewController {
+class LoadingViewController: UIViewController, CAAnimationDelegate {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    private func transitionToNews() {
+        DispatchQueue.main.async {
+            guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "tab_bar_controller") else {
+                return
+            }
+            self.controllerSwitcher?.setViewController(vc, animator: FadeTransition())
+        }
+    }
+    
+    private func animateLogo() {
+        let alphaAnimation = CAKeyframeAnimation(keyPath: "opacity")
+        alphaAnimation.duration = 1.8
+        alphaAnimation.values = [0, 1, 1, 0]
+        alphaAnimation.keyTimes = [0, 0.05, 0.90, 1]
+        alphaAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+        
+        let positionAnimation = CAKeyframeAnimation(keyPath: "transform.translation.y")
+        positionAnimation.duration = 0.5
+        positionAnimation.values = [-15, 0]
+        positionAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        
+        let groupAnimation = CAAnimationGroup()
+        groupAnimation.animations = [positionAnimation, alphaAnimation]
+        groupAnimation.fillMode = kCAFillModeForwards
+        groupAnimation.isRemovedOnCompletion = false
+        groupAnimation.duration = 1.8
+        groupAnimation.delegate = self
+        logoImageView.layer.add(groupAnimation, forKey: "group")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let trendingService = NewsService()
-        let _ = trendingService.requestTrendingTopicsWithDate(Date(), count: 6, success: { (topics) in
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
-                guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "tab_bar_controller") as? InitialTabBarController else {
-                    return
-                }
-                
-                vc.trendingTopics = topics
-                self.controllerSwitcher?.setViewController(vc, animator: FadeTransition())
-            })
-            
-        }, fail: { (error) in
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6, execute: {
-                guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "tab_bar_controller") else {
-                    return
-                }
-                self.controllerSwitcher?.setViewController(vc, animator: FadeTransition())
-            })
-        })
+        animateLogo()
+    }
+    
+    // MARK: CAAnimationDelegate
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        transitionToNews()
     }
 }
