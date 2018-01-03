15c27bb240c68bff7fe4ba1ad6c8f9a0036c80e8
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2018-01-03 00:57:25 -0300

    feat: updated snapshots

--- Headlines/src/ViewModels/ReviewBannerViewModel.swift
+++ Headlines/src/ViewModels/ReviewBannerViewModel.swift
@@ -13,6 +13,10 @@ class ReviewBannerViewModel: NSObject {
     let userManager = UserSettingsManager()
     
     func shouldShowBanner() -> Bool {
+        if !ProcessInfo.processInfo.arguments.contains("mockReviewBanner") {
+            return true
+        }
+        
         guard let firstOpenDate = userManager.firstOpenDate else {
             return false
         }
